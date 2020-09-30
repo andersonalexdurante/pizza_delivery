58ee5b07d8aec7ba75d0b74d43d9c1982e0d5dab
andersonalexdurante <anderson.alexdurante@gmail.com>
2020-09-30 17:47:43 -0300

    criando api com aqueduct

---
+++ pizza_delivery_api/lib/pizza_delivery_api.dart
@@ -0,0 +1,11 @@
+/// pizza_delivery_api
+///
+/// A Aqueduct web server.
+library pizza_delivery_api;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
